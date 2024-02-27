45e70e7999268166bef299c69f69e5658cb1edfc
huzaifabashir1727 <137119496+huzaifabashir1727@users.noreply.github.com>
2024-02-27 16:27:35 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+        13:'3.25rem'
+      },
+      fontSize:{
+        '10xl': ['9rem', { lineHeight: '1.2' }],
+      },
+      fontWeight:{
+        between:'150'
+      }
+    },
+  },
+  plugins: [],
+}
+
